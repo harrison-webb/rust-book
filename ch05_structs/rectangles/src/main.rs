9b01b2e1486c73548faa547a8137f47a65ed98ec
Harrison Webb <harrison.dev.00@gmail.com>
2023-07-26 00:39:56 -0600

    rectangle program with struct + derived trait + learned about dbg! macro

--- ch05_structs/rectangles/src/main.rs
+++ ch05_structs/rectangles/src/main.rs
@@ -1,18 +1,25 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
 fn main() {
+    let scale = 2;
     let rect1 = Rectangle {
-        width: 30,
-        height: 50,
+        width: dbg!(30 * scale), // tells me exactly what is going on here in the program output!
+        height: 50 * scale,
     };
 
     println!(
         "The area of the rectangle is {} square pixels.",
         area(&rect1)
     );
+
+    // {:?} says use the Debug output format
+    println!("rect1 is {:?}", rect1);
+
+    dbg!(&rect1);
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
